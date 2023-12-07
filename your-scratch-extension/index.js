ca84edab798baa3ee42778604f0e388182d44d84
commandblocks0 <153120669+commandblocks0@users.noreply.github.com>
2023-12-07 13:17:54 +0000

    Update

--- your-scratch-extension/index.js
+++ your-scratch-extension/index.js
@@ -2,99 +2,72 @@ const BlockType = require('../../extension-support/block-type');
 const ArgumentType = require('../../extension-support/argument-type');
 const TargetType = require('../../extension-support/target-type');
 
-class Scratch3YourExtension {
+class Scratch3CommandBlocks0 {
 
-    constructor (runtime) {
-        // put any setup for your extension here
+    constructor(runtime) {
+        // No additional setup needed for this simplified version
     }
 
-    /**
-     * Returns the metadata about your extension.
-     */
-    getInfo () {
+    getInfo() {
         return {
-            // unique ID for your extension
-            id: 'yourScratchExtension',
-
-            // name that will be displayed in the Scratch UI
-            name: 'Demo',
-
-            // colours to use for your extension blocks
-            color1: '#000099',
-            color2: '#660066',
-
-            // icons to display
-            blockIconURI: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAkAAAAFCAAAAACyOJm3AAAAFklEQVQYV2P4DwMMEMgAI/+DEUIMBgAEWB7i7uidhAAAAABJRU5ErkJggg==',
-            menuIconURI: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAkAAAAFCAAAAACyOJm3AAAAFklEQVQYV2P4DwMMEMgAI/+DEUIMBgAEWB7i7uidhAAAAABJRU5ErkJggg==',
-
-            // your Scratch blocks
+            id: 'commandblocks0',
+            name: 'commandblocks0',
+            color1: '#000000',
+            color2: '#000000',
+            blockIconURI: null,
+            menuIconURI: null,
             blocks: [
                 {
-                    // name of the function where your block code lives
-                    opcode: 'myFirstBlock',
-
-                    // type of block - choose from:
-                    //   BlockType.REPORTER - returns a value, like "direction"
-                    //   BlockType.BOOLEAN - same as REPORTER but returns a true/false value
-                    //   BlockType.COMMAND - a normal command block, like "move {} steps"
-                    //   BlockType.HAT - starts a stack if its value changes from false to true ("edge triggered")
+                    opcode: 'textToNumbers',
                     blockType: BlockType.REPORTER,
-
-                    // label to display on the block
-                    text: 'My first block [MY_NUMBER] and [MY_STRING]',
-
-                    // true if this block should end a stack
+                    text: 'Text to Numbers [TEXT]',
                     terminal: false,
-
-                    // where this block should be available for code - choose from:
-                    //   TargetType.SPRITE - for code in sprites
-                    //   TargetType.STAGE  - for code on the stage / backdrop
-                    // remove one of these if this block doesn't apply to both
-                    filter: [ TargetType.SPRITE, TargetType.STAGE ],
-
-                    // arguments used in the block
+                    filter: [TargetType.SPRITE, TargetType.STAGE],
                     arguments: {
-                        MY_NUMBER: {
-                            // default value before the user sets something
-                            defaultValue: 123,
-
-                            // type/shape of the parameter - choose from:
-                            //     ArgumentType.ANGLE - numeric value with an angle picker
-                            //     ArgumentType.BOOLEAN - true/false value
-                            //     ArgumentType.COLOR - numeric value with a colour picker
-                            //     ArgumentType.NUMBER - numeric value
-                            //     ArgumentType.STRING - text value
-                            //     ArgumentType.NOTE - midi music value with a piano picker
-                            type: ArgumentType.NUMBER
+                        TEXT: {
+                            defaultValue: '',
+                            type: ArgumentType.STRING,
                         },
-                        MY_STRING: {
-                            // default value before the user sets something
-                            defaultValue: 'hello',
-
-                            // type/shape of the parameter - choose from:
-                            //     ArgumentType.ANGLE - numeric value with an angle picker
-                            //     ArgumentType.BOOLEAN - true/false value
-                            //     ArgumentType.COLOR - numeric value with a colour picker
-                            //     ArgumentType.NUMBER - numeric value
-                            //     ArgumentType.STRING - text value
-                            //     ArgumentType.NOTE - midi music value with a piano picker
-                            type: ArgumentType.STRING
-                        }
-                    }
-                }
-            ]
+                    },
+                },
+                {
+                    opcode: 'numbersToText',
+                    blockType: BlockType.REPORTER,
+                    text: 'Numbers to Text [NUMBERS]',
+                    terminal: false,
+                    filter: [TargetType.SPRITE, TargetType.STAGE],
+                    arguments: {
+                        NUMBERS: {
+                            defaultValue: '',
+                            type: ArgumentType.NUMBERS,
+                        },
+                    },
+                },
+            ],
         };
     }
 
+    textToNumbers({ TEXT }) {
+        let output = '';
 
-    /**
-     * implementation of the block with the opcode that matches this name
-     *  this will be called when the block is used
-     */
-    myFirstBlock ({ MY_NUMBER, MY_STRING }) {
-        // example implementation to return a string
-        return MY_STRING + ' : doubled would be ' + (MY_NUMBER * 2);
+        for (let i = 0; i < TEXT.length; i++) {
+            const currentChar = TEXT.charCodeAt(i).toString().padStart(3, '0');
+            output += currentChar;
+        }
+
+        return output;
+    }
+
+    numbersToText({ NUMBERS }) {
+        let output = '';
+
+        for (let i = 0; i < NUMBERS.length; i += 3) {
+            const currentNum = parseInt(NUMBERS.substr(i, 3), 10);
+            output += String.fromCharCode(currentNum);
+        }
+
+        return output;
     }
 }
 
-module.exports = Scratch3YourExtension;
+module.exports = Scratch3CommandBlocks0;
